39e478cd60812c7166de27a9204cdad4f7390561
MoFaisal136 <shahansha.faisal@gmail.com>
2022-12-21 09:51:25 -0500

    the basic solar system with the sun added is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        solarSystem Xron = new solarSystem();
+        Xron.moon=2;
+        Xron.sun=2;
+        Xron.moon=1;
+
+        FeatureSun Fxron= new FeatureSun();
+        Fxron.color="green";
+        Fxron.size="438272km";
+        Fxron.temp="23478274372k";
+
+
+    }
+}
